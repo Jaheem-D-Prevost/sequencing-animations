d7bad2959b9e82357649b2000bd8554e62145066
jaheemprevost <jaheemdp@gmail.com>
2023-04-13 21:48:31 -0400

    Complete sequencing animations exercise

--- main.js
+++ main.js
@@ -12,3 +12,68 @@ const aliceTiming = {
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+
+// 1 call backs
+
+// const animationObject1 = alice1.animate(aliceTumbling, aliceTiming);
+// animationObject1.finished
+// .then(() => {
+//   const animationObject2 = alice2.animate(aliceTumbling, aliceTiming);
+
+//   animationObject2.finished
+//   .then(() => {
+//     const animationObject3 = alice3.animate(aliceTumbling, aliceTiming);
+    
+//     animationObject3.finished
+//     .then(() => {
+//       console.log('finished.');
+//       return 0;
+//     })
+//     .catch(err => console.log(err));
+//   })
+//   .catch(err => console.log(err));
+// })
+// .catch(err => console.log(err)); 
+
+// 2 promise chaining
+
+// const animationObject1 = alice1.animate(aliceTumbling, aliceTiming);
+
+// animationObject1.finished
+// .then(() => {
+//   return alice2.animate(aliceTumbling, aliceTiming).finished;
+// })
+// .then(secondAnimationFinished => {
+//   return alice3.animate(aliceTumbling, aliceTiming).finished;
+// })
+// .then(thirdAnimationFinished => {
+//   console.log('finished');
+//   return 0;
+// })
+// .catch(err => console.log(err));
+
+// 3 async await
+
+// const animate = async () => {
+//   try {
+//     const animationObject1 = alice1.animate(aliceTumbling, aliceTiming);
+
+//     const firstAnimationFinished = await animationObject1.finished;
+
+//     const animationObject2 = alice2.animate(aliceTumbling, aliceTiming);
+
+//     const secondAnimationFinished = await animationObject2.finished;
+
+//     const animationObject3 = alice3.animate(aliceTumbling, aliceTiming);
+
+//     const thirdAnimationFinished = await animationObject3.finished;
+
+//     console.log('finished');
+//     return 0;
+//   } catch(err) {
+//     console.log(err);
+//   }
+// };
+
+// animate();
